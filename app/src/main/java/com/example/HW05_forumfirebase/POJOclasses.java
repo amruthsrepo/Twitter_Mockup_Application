ddba7592fb17ee0be14f4ed971f8a648573fa935
Amruth Nag <51161103+amruthsrepo@users.noreply.github.com>
2021-11-01 20:47:32 -0400

    Fix null pointer for likes 2

--- app/src/main/java/com/example/HW05_forumfirebase/POJOclasses.java
+++ app/src/main/java/com/example/HW05_forumfirebase/POJOclasses.java
@@ -106,75 +106,78 @@ public class POJOclasses {
         }
 
         public Set<String> getLikes() {
-            return likes == null ? likes : new Set<String>() {
-                @Override
-                public int size() {
-                    return 0;
-                }
+            if (likes == null) {
+                likes = new Set<String>() {
+                    @Override
+                    public int size() {
+                        return 0;
+                    }
 
-                @Override
-                public boolean isEmpty() {
-                    return false;
-                }
+                    @Override
+                    public boolean isEmpty() {
+                        return false;
+                    }
 
-                @Override
-                public boolean contains(@Nullable Object o) {
-                    return false;
-                }
+                    @Override
+                    public boolean contains(@Nullable Object o) {
+                        return false;
+                    }
 
-                @NonNull
-                @Override
-                public Iterator<String> iterator() {
-                    return null;
-                }
+                    @NonNull
+                    @Override
+                    public Iterator<String> iterator() {
+                        return null;
+                    }
 
-                @NonNull
-                @Override
-                public Object[] toArray() {
-                    return new Object[0];
-                }
+                    @NonNull
+                    @Override
+                    public Object[] toArray() {
+                        return new Object[0];
+                    }
 
-                @NonNull
-                @Override
-                public <T> T[] toArray(@NonNull T[] ts) {
-                    return null;
-                }
+                    @NonNull
+                    @Override
+                    public <T> T[] toArray(@NonNull T[] ts) {
+                        return null;
+                    }
 
-                @Override
-                public boolean add(String s) {
-                    return false;
-                }
+                    @Override
+                    public boolean add(String s) {
+                        return false;
+                    }
 
-                @Override
-                public boolean remove(@Nullable Object o) {
-                    return false;
-                }
+                    @Override
+                    public boolean remove(@Nullable Object o) {
+                        return false;
+                    }
 
-                @Override
-                public boolean containsAll(@NonNull Collection<?> collection) {
-                    return false;
-                }
+                    @Override
+                    public boolean containsAll(@NonNull Collection<?> collection) {
+                        return false;
+                    }
 
-                @Override
-                public boolean addAll(@NonNull Collection<? extends String> collection) {
-                    return false;
-                }
+                    @Override
+                    public boolean addAll(@NonNull Collection<? extends String> collection) {
+                        return false;
+                    }
 
-                @Override
-                public boolean retainAll(@NonNull Collection<?> collection) {
-                    return false;
-                }
+                    @Override
+                    public boolean retainAll(@NonNull Collection<?> collection) {
+                        return false;
+                    }
 
-                @Override
-                public boolean removeAll(@NonNull Collection<?> collection) {
-                    return false;
-                }
+                    @Override
+                    public boolean removeAll(@NonNull Collection<?> collection) {
+                        return false;
+                    }
 
-                @Override
-                public void clear() {
+                    @Override
+                    public void clear() {
 
-                }
-            };
+                    }
+                };
+            }
+            return likes;
         }
 
         public void setLikes(Set<String> likes) {
